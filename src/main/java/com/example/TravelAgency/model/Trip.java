28995cba3ce0d05139ade6f7aad3d0824eb441e3
Krzysztof Mikolajski <krzysztof.mikolajski1@gmail.com>
2020-10-13 18:17:04 +0200

    First entity (trip) and repository created.

---
+++ src/main/java/com/example/TravelAgency/model/Trip.java
@@ -0,0 +1,61 @@
+package com.example.TravelAgency.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "trips")
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message="Trip's destination mast not be empty")
+    private String destination;
+    @NotBlank(message="Trip's description mast not be empty")
+    private String description;
+    private int price;
+    private boolean available;
+
+    public Trip() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
