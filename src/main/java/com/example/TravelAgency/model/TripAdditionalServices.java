2f2fdb3d039306d89fbcf7acba966a246adb96b0
Krzysztof Mikolajski <krzysztof.mikolajski1@gmail.com>
2020-11-17 12:09:23 +0100

    New entity added.

---
+++ src/main/java/com/example/TravelAgency/model/TripAdditionalServices.java
@@ -0,0 +1,54 @@
+package com.example.TravelAgency.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+
+@Entity
+@Table(name = "additional_services")
+public class TripAdditionalServices {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message="Trip's additional services description mast not be empty")
+    private String description;
+    private int price;
+    private boolean available;
+
+    public TripAdditionalServices() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+}
