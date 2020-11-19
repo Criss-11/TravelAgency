5e257a4db529337d873f0a56cc73e55124a97902
Krzysztof Mikolajski <krzysztof.mikolajski1@gmail.com>
2020-11-19 21:42:53 +0100

    N+1 selects -> solution

--- src/main/java/com/example/TravelAgency/adapter/SqlTripAdditionalServicesRepository.java
+++ src/main/java/com/example/TravelAgency/adapter/SqlTripAdditionalServicesRepository.java
@@ -3,9 +3,14 @@ package com.example.TravelAgency.adapter;
 import com.example.TravelAgency.model.TripAdditionalServices;
 import com.example.TravelAgency.model.TripAdditionalServicesRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public interface SqlTripAdditionalServicesRepository extends TripAdditionalServicesRepository, JpaRepository<TripAdditionalServices, Integer>{
-
+    @Override
+    @Query("from TripAdditionalServices g join fetch g.trips")
+    List<TripAdditionalServices> findAll();
 }
